ce2a4c889a3b9c438f0f70675d7dd30cc52ef7cb
Waiizen <nathanferrer.nf@gmail.com>
2021-03-09 10:29:38 +0100

    Add nbRep / Patch disabling things 🐛

--- index.js
+++ index.js
@@ -2,39 +2,52 @@ var secExercice = 0;
 var secWait = 0;
 var sec = 0;
 var nbRep = 1;
+var nbRepTot = 0;
 var prepTime = 10;
 var totalTimeSec = 0;
 var totalTimeMin = 0;
 var lock=false;
 
 function increaseExe(){
-	document.getElementById('setExerciceSec').value = parseInt(document.getElementById('setExerciceSec').value)+1;
-	updateTimer();
+	if(!lock){
+		document.getElementById('setExerciceSec').value = parseInt(document.getElementById('setExerciceSec').value)+1;
+		updateTimer();		
+	}
 }
 
 function decreaseExe(){
-	if(parseInt(document.getElementById('setExerciceSec').value) > 0) document.getElementById('setExerciceSec').value = parseInt(document.getElementById('setExerciceSec').value)-1;
-	updateTimer();
+	if(!lock){
+		if(parseInt(document.getElementById('setExerciceSec').value) > 0) document.getElementById('setExerciceSec').value = parseInt(document.getElementById('setExerciceSec').value)-1;
+		updateTimer();
+	}
 }
 
 function increaseRest(){
-	document.getElementById('setWaitSec').value = parseInt(document.getElementById('setWaitSec').value)+1;
-	updateTimer();
+	if(!lock){
+		document.getElementById('setWaitSec').value = parseInt(document.getElementById('setWaitSec').value)+1;
+		updateTimer();	
+	}	
 }
 
 function decreaseRest(){
-	if(parseInt(document.getElementById('setWaitSec').value) > 0) document.getElementById('setWaitSec').value = parseInt(document.getElementById('setWaitSec').value)-1;
-	updateTimer();
+	if(!lock){
+		if(parseInt(document.getElementById('setWaitSec').value) > 0) document.getElementById('setWaitSec').value = parseInt(document.getElementById('setWaitSec').value)-1;
+		updateTimer();
+	}
 }
 
 function increaseRep(){
-	document.getElementById('setNbRep').value = parseInt(document.getElementById('setNbRep').value)+1;
-	updateTimer();
+	if(!lock){
+		document.getElementById('setNbRep').value = parseInt(document.getElementById('setNbRep').value)+1;
+		updateTimer();
+	}
 }
 
 function decreaseRep(){
-	if(parseInt(document.getElementById('setNbRep').value) > 1) document.getElementById('setNbRep').value = parseInt(document.getElementById('setNbRep').value)-1;
-	updateTimer();
+	if(!lock){
+		if(parseInt(document.getElementById('setNbRep').value) > 1) document.getElementById('setNbRep').value = parseInt(document.getElementById('setNbRep').value)-1;
+		updateTimer();
+	}	
 }
 
 function reset(){
@@ -49,6 +62,7 @@ function reset(){
 	document.getElementById('setExerciceSec').value = 0;
 	document.getElementById('setWaitSec').value = 0;
 	document.getElementById('setNbRep').value = 1;
+	nbRepTot=0;
 	updateTimer();
 }
 
@@ -62,15 +76,25 @@ function updateTimer(){
 	totalTimeSec = totalTimeSec % 60;
 
 	document.getElementById('timerText').innerHTML = (totalTimeMin.toString().length < 2 ? ('0'+totalTimeMin) : totalTimeMin) + ':' + (totalTimeSec.toString().length < 2 ? ('0'+totalTimeSec) : totalTimeSec);
-
+	document.getElementById('repTot').innerHTML = (nbRep.toString().length < 2 ? ('0'+nbRep) : nbRep);
+	document.getElementById('repNb').innerHTML = (nbRepTot.toString().length < 2 ? ('0'+nbRepTot) : nbRepTot);
 }
 
 function setTimer(){
 	if(!lock){
 		lock=true;
 		document.getElementById('btnGo').disabled = true;
-	document.getElementById('btnGo').classList.remove("btn-success");
-	document.getElementById('btnGo').classList.add("btn-secondary");
+		document.getElementById('btnGo').classList.remove("btn-success");
+		document.getElementById('btnGo').classList.add("btn-secondary");
+		document.getElementById('setExerciceSec').readOnly = true;
+		document.getElementById('setWaitSec').readOnly = true;
+		document.getElementById('setNbRep').readOnly = true;
+
+		var list = document.getElementsByClassName("onlyLilCss");
+		for (index = 0; index < list.length; ++index) {
+    		list[index].setAttribute("disabled", "");
+		}
+
 		secExercice = parseInt(document.getElementById('setExerciceSec').value);
 		secWait = parseInt(document.getElementById('setWaitSec').value);
 		nbRep = parseInt(document.getElementById('setNbRep').value);
@@ -79,7 +103,6 @@ function setTimer(){
 		document.getElementById('timerTitle').innerHTML = 'PREPARATION';
 		document.body.style.backgroundColor = "#f1ae89";
 		var countdownPreparation = setInterval(prepareTimer, 1000);
-
 		function prepareTimer(){
 			document.getElementById('timerText').innerHTML = '00:' + (prepTime.toString().length < 2 ? ('0'+prepTime) : prepTime);
 			prepTime = prepTime - 1;
@@ -95,6 +118,9 @@ function exerciceInterval(){
 	sec = secExercice;
 	document.getElementById('timerTitle').innerHTML = 'WORK';
 	document.body.style.backgroundColor = "#9dad7f";
+	nbRepTot++;
+	document.getElementById('repNb').innerHTML = (nbRepTot.toString().length < 2 ? ('0'+nbRepTot) : nbRepTot);
+
 	var countdown1 = setInterval(timer, 1000);
 
 	function timer(){
@@ -136,4 +162,14 @@ function finish(){
 	document.getElementById('btnGo').disabled = false;
 	document.getElementById('btnGo').classList.remove("btn-secondary");
 	document.getElementById('btnGo').classList.add("btn-success");
+	document.getElementById('setExerciceSec').readOnly = false;
+	document.getElementById('setWaitSec').readOnly = false;
+	document.getElementById('setNbRep').readOnly = false;
+
+	var list = document.getElementsByClassName("onlyLilCss");
+	for (index = 0; index < list.length; ++index) {
+    	list[index].removeAttribute("disabled", "");
+	}
+
+	nbRepTot=0;
 }
